98b97adc022a481b2e5c03f6b6cd7e033574c88f
Ahmed Warsama <ahmedwarsama@hotmail.co.uk>
2024-03-31 23:13:08 +0100

    Comments added and variable name changed.

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,4 @@
-name = input("What is your name?")
-print(f"Your name is {name}")
+# This program will ask the user for their name and then print it out.
+# Variable renamed from name to answer. 
+answer = input("What is your name?")
+print(f"Your name is {answer}")
